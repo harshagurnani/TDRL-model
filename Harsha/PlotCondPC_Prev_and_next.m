c61bdc742bf35eba4b24898dfe6c1749c8754e8d
Harsha Gurnani <harsha.gurnani.16@ucl.ac.uk>
2017-05-29 10:28:41 +0100

    plot changes

--- Harsha/PlotCondPC_Prev_and_next.m
+++ Harsha/PlotCondPC_Prev_and_next.m
@@ -1,4 +1,4 @@
-function PlotCondPC_Mice_Model(data_mice, data_model, action, correct, varargin)
+function PlotCondPC_Prev_and_next(data_mice, varargin)
 
 color=[0 0 1; 1 0 0; 0 1 0; 0 0 0];   % blue, red, green, black
 blockLabels ={'DA-L', 'DA-R', 'DA-Both', 'No DA'};
@@ -6,33 +6,26 @@ markerShapes={'o', 's', 'v', '^'};
 
 % changing y-axis values
 data_mice(:,3) = (1 + data_mice(:,3)) ./ 2;
-if ~isempty(data_model)
-data_model(:,22) = (1 + data_model(:,22)) ./ 2;
-end
-action = (1+action)/2;
-
 
 contrasts = unique(data_mice(:,2))';
-per_mice = nan(2,length(contrasts));
-per_model = nan(2,length(contrasts));
+per_mice_next = nan(2,length(contrasts));
+per_mice_prev = nan(2,length(contrasts));
 
-if nargin >4
+if nargin >1
     includeContrast = varargin{1}; 
 else
     includeContrast = ones(length(contrasts)*length(unique(data_mice(:,8))),1);
 end
 
-niters = size(action,2);
-if nargin>5
+if nargin>2
     blocks = varargin{2};
 else
     blocks=unique(data_mice(:,8))';
 end
 
-% if nargin>6
-    fig = figure;%varargin{3};
-    ax = axes(fig,'position',[0.09,0.1,0.86,0.88],'FontSize',16); hold on 
-% end
+fig = figure;%varargin{3};
+ax = axes(fig,'position',[0.09,0.1,0.86,0.88],'FontSize',16); hold on 
+
 
 b=0;
 for blockID = blocks
@@ -42,26 +35,18 @@ for blockID = blocks
       
       id=[false;data_mice(2:end,2)==istim & data_mice(2:end,8)==blockID & ...
           data_mice(1:end-1,3)==0 & data_mice(1:end-1,10)==1];
-      per_mice(1,c)= nanmean(data_mice(id,3)) ;
+      per_mice_next(1,c)= nanmean(data_mice(id,3)) ;
+      id=[data_mice(1:end-1,2)==istim & data_mice(1:end-1,8)==blockID & ...
+          data_mice(2:end,3)==0 & data_mice(2:end,10)==1; false];
+      per_mice_prev(1,c)= nanmean(data_mice(id,3)) ;
+      
       id=[false;data_mice(2:end,2)==istim & data_mice(2:end,8)==blockID & ...
           data_mice(1:end-1,3)==1 & data_mice(1:end-1,10)==1];
-      per_mice(2,c)= nanmean(data_mice(id,3)) ;
-      
-      
-      per_model(1,c)=0;
-      per_model(2,c)=0;
-      
-      for iter = 1:niters
-          id =[false;data_model(2:end,2)==istim & data_model(2:end,8)==blockID & ...
-             action(1:end-1,iter)==0 & correct(1:end-1,iter)==1];
-          per_model(1,c)= per_model(1,c) + nanmean(action( id,iter)) ;
-          id = [false; data_model(2:end,2)==istim & data_model(2:end,8)==blockID & ...
-             action(1:end-1,iter)==1 & correct(1:end-1,iter)==1];
-          per_model(2,c)= per_model(2,c) + nanmean(action( id,iter)) ;
-      end
-      per_model(1,c)=per_model(1,c)/niters;
-      per_model(2,c)=per_model(2,c)/niters;
-      
+      per_mice_next(2,c)= nanmean(data_mice(id,3)) ;
+      id=[data_mice(1:end-1,2)==istim & data_mice(1:end-1,8)==blockID & ...
+          data_mice(2:end,3)==1 & data_mice(2:end,10)==1; false];
+      per_mice_prev(2,c)= nanmean(data_mice(id,3)) ;
+     
       c=c+1;
    end
 
@@ -74,29 +59,25 @@ for blockID = blocks
 %    ax = axes(fig(blockID),'position',[0.09,0.1,0.86,0.88],'FontSize',16); hold on   
 %    suptitle(sprintf('Psychomeric curve conditional to previous correct choice for Block %s',blockLabels{blockID})) 
 %    end
-   if nargin==8
-       hold on;
-       subplot(varargin{4}(1), varargin{4}(2), varargin{4}(3))
-       hold on;
-   end
+
    %plot
-   plot(ax,contrasts(plot_contrast),per_mice(1,plot_contrast),...
-      'color',color(blockID,:),...
+   plot(ax,contrasts(plot_contrast),per_mice_next(1,plot_contrast),...
+      'color','b',...
       'marker',markerShapes{blockID},'markersize',10,'markeredgecolor','b',...
-      'markerfacecolor','b','linestyle','none',...
+      'markerfacecolor','b','linestyle','-',...
       'linewidth',1.2);
-   plot(ax,contrasts(plot_contrast),per_mice(2,plot_contrast),...
-      'color',color(blockID,:),...
+   plot(ax,contrasts(plot_contrast),per_mice_next(2,plot_contrast),...
+      'color','r',...
       'marker',markerShapes{blockID},'markersize',10,'markeredgecolor','r',...
-      'markerfacecolor','r','linestyle','none',...
+      'markerfacecolor','r','linestyle','-',...
       'linewidth',1.2);
 
    
    % plot the model
-   plot(ax,contrasts(plot_contrast),per_model(1,plot_contrast),'color','b',...
+   plot(ax,contrasts(plot_contrast),per_mice_prev(1,plot_contrast),'color','b',...
       'marker',markerShapes{blockID},'markersize',10,'markeredgecolor','b',...
       'linestyle','--','linewidth',1.2);
-   plot(ax,contrasts(plot_contrast),per_model(2,plot_contrast),'color','r',...
+   plot(ax,contrasts(plot_contrast),per_mice_prev(2,plot_contrast),'color','r',...
       'marker',markerShapes{blockID},'markersize',10,'markeredgecolor','r',...
       'linestyle','--','linewidth',1.2);
 
@@ -134,7 +115,7 @@ end
       'markerfacecolor','k','linestyle','none');
       b=b+1;
    end
-   legend(h, 'After correct L','After correct R','Mouse','Model',blockLabels{blocks},'Location','southeast');
+   legend(h, 'Condtioned to correct L','Condtioned to correct R','After Correct choice','Before correct choice',blockLabels{blocks},'Location','southeast');
 % end
 
 
