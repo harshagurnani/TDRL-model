87f21fcf88b8476190cb07acfd1159c242843fc0
Harsha Gurnani <harsha.gurnani.16@ucl.ac.uk>
2017-06-07 17:56:04 +0100

    dummy run

--- Harsha/DummyRun.m
+++ Harsha/DummyRun.m
@@ -1,4 +1,4 @@
-function [ TrialStimuli, TrialBlocks, action, correct ] = DummyRun( params )
+function [ TrialStimuli, TrialBlocks, action, correct, QL, QR ] = DummyRun( params )
 % Run model with parameters for fake stimuli
 % shifts after diff/easy stimuli
 
@@ -9,20 +9,20 @@ noiseSTD = params(3);
 Qbias = params(4);
 
 
-stimuli = [-0.5 -0.25 -0.12 -0.05 0 0.05 0.12 0.25 0.5];
+stimuli = [-0.5 -0.25 -0.12 -0.05 0 0.05 0.12 0.25 0.5]';
 nStimuli = length(stimuli);
-nTrials = 10000;
+nTrials = 30000;
 nIters = 99;
 
 %Generate random stims
-TrialStimuli = stimuli( unidrnd( nStimuli, [nTrials,nIters]) );
+TrialStimuli = stimuli( unidrnd( nStimuli, [nTrials,1]) );
 
 blocks = [1,2];
 nBlocks = length(blocks);
 
 %Create blocks
 TrialBlocks = ones(nTrials,1);
-TrialsPerSess = 700;
+TrialsPerSess = 500;
 nSess = floor(nTrials/(TrialsPerSess * nBlocks));
 for jj=1:nSess
    blockID = blocks(1+mod(jj,nBlocks));
@@ -36,13 +36,28 @@ BlockReward = [1+DA_val,     1   , 1+DA_val, 1;
 
 action = nan( nTrials, nIters);
 correct = zeros(nTrials, nIters);
+newDay = zeros( nTrials,1);
+for jj=2001:nTrials
+   if sum(newDay(jj-1000:jj-1)) == 0
+       if rand<0.003
+          newDay(jj) = 1;  
+       end
+   end
+end
 
-QLL = 1; QLR = 1; QRL = 1; QRR = 1;
+Bstimuli = [min(stimuli):0.02:max(stimuli)];
+
+Bstimuli = [-1:0.02:1];
+Bstimuli  = stimuli;
 QL = nan(nTrials, nIters); QR = QL;
 
 % Run model
 for iter = 1:nIters
+   QLL = 1; QLR = 1; QRL = 1; QRR = 1;
    for trial = 1:nTrials
+      if newDay(trial) == 1
+          QLL = 1; QLR = 1; QRL = 1; QRR = 1;
+      end
       BlockID = TrialBlocks(trial);
       reward = BlockReward(:, BlockID);
       
@@ -50,26 +65,26 @@ for iter = 1:nIters
       rewardGiven = 0;
       
       %Internal noisy representation of stimulus
-      stimResp = randn*noiseSTD + TrialStimuli(trial, iter);
+      stimResp = randn*noiseSTD + TrialStimuli(trial);
       
       %Belief state
-      Belief = normpdf(stimuli,stimResp,noiseSTD);
+      Belief = normpdf(Bstimuli,stimResp,noiseSTD);
       Belief = Belief./sum(Belief);
       
-      Belief_L = sum(Belief(stimuli<0));
-      Belief_R = sum(Belief(stimuli>=0));
+      Belief_L = sum(Belief(Bstimuli<0));
+      Belief_R = sum(Belief(Bstimuli>=0));
       
       % Q-values "Expected reward"
       QL(trial, iter) = Belief_L * QLL + Belief_R * QRL;
-      QR(trial, iter) = Belief_L * QLR + Belief_R * QRR     + Qbias;
+      QR(trial, iter) = Belief_L * QLR + Belief_R * QRR     ;
       
       % Action
       
-      if QL(trial, iter) > QR(trial, iter)
+      if QL(trial, iter) > QR(trial, iter) + Qbias
           % left more valuable
           action(trial, iter) = -1;
           
-      elseif QL(trial, iter) < QR(trial, iter)
+      elseif QL(trial, iter) < QR(trial, iter) + Qbias
           % right more valuable
           action(trial, iter) = 1;
           
@@ -84,18 +99,18 @@ for iter = 1:nIters
       
       
       % correct or error
-      if TrialStimuli(trial, iter) < 0  &&  action(trial, iter) == -1
+      if TrialStimuli(trial) < 0  &&  action(trial, iter) == -1
         %Correct left action
         correct( trial, iter) = 1;
         rewardGiven = reward(1);
             
          
-      elseif TrialStimuli(trial, iter) > 0 && action(trial, iter) == 1
+      elseif TrialStimuli(trial) > 0 && action(trial, iter) == 1
         % Correct Right action
         correct(trial, iter) = 1;
         rewardGiven = reward(2);
         
-      elseif TrialStimuli(trial, iter) == 0
+      elseif TrialStimuli(trial) == 0
          if rand() < 0.5
              % Correct randomly
              rewardGiven =  reward( 1+ (action(trial,iter)==1) );
